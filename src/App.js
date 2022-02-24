7beb730284c49f2ecd4e9dd2551d48c6ed372a4a
Adrijana <adrijana.jovicic@gmail.com>
2022-02-24 16:59:35 +0100

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './component/Home';
 import Navbar from './component/Navbar';
+import Products from './component/Products';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home/>
-    </>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
   );
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -12,10 +13,10 @@ export default function Navbar() {
                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                           <li className="nav-item">
-                              <a className="nav-link active" aria-current="page" href={{}}>Home</a>
+                              <Link to='/' className="nav-link active" aria-current="page" >Home</Link>
                           </li>
                           <li className="nav-item">
-                              <a className="nav-link" href={{}}>Products</a>
+                              <Link to='/products' className="nav-link" >Products</Link>
                           </li>
                           <li className="nav-item">
                               <a className="nav-link" href={{}}>About</a>
